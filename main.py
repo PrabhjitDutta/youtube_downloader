19590d9b2edd21f8e324a0b38391bd49e0f348dd
PrabhitDutta <duttaprabhjit6@gmail.com>
2018-04-03 22:50:43 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+from pytube import YouTube
+
+search = "https://www.youtube.com/results"
+search_term = input("Enter the name of the Video: ")
+params = {"search_query" : "youtube " + search_term}
+
+r = requests.get(search, params=params)
+soup = BeautifulSoup(r.text, 'html5lib')
+
+result = soup.find('a', {'aria-hidden': 'true'})
+result = result['href']
+
+YouTube("https://www.youtube.com" + result).streams.first().download()
